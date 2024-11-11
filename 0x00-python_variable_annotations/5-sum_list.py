6425864bf2286c45a4538d6c89f65c340c195eb7
Dev-Kings <kingasiadavid41@gmail.com>
2024-11-11 16:01:23 +0300

    Explored type annotation for function taking list of integers and returns their sum

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Module containing function integer summing elements of a list.
+"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """Returns the sum of a list of floats."""
+    return sum(input_list)
