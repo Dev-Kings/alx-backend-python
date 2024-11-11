7479ee606d3f79d82d93eef4ac92def1f6bab0bd
Dev-Kings <kingasiadavid41@gmail.com>
2024-11-11 15:36:35 +0300

    Explored type annotating a function concatenating 2 string arguments

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Module with function concatenating 2 strings.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Concatenates two strings str1 and str2."""
+    return str1 + str2
